a8081a4bcdbba73317378d12a4774fd40f8ea979
Ben Zatrok <ben@amberglass.co>
2024-03-13 12:31:28 +0100

    app router disabled

--- PodcastGPT.Frontend/pages/_app.tsx
+++ PodcastGPT.Frontend/pages/_app.tsx
@@ -1,12 +1,21 @@
 import Footer from '@/components/Generic/Footer';
+import { SiteHeader } from '@/components/Generic/SiteHeader';
+import { TailwindIndicator } from '@/components/Generic/TailwindIndicator';
+import { ThemeProvider } from '@/components/Generic/ThemeProvider';
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <Component {...pageProps} />
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+      <div className="relative flex min-h-screen flex-col">
+        <SiteHeader />
+        <div className="flex-1">
+          <Component {...pageProps} />
+        </div>
+      </div>
+      <TailwindIndicator />
       <Footer />
-    </>
+    </ThemeProvider>
   );
 }
